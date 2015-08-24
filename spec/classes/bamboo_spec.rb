bea5c187ae4aeadc9a5613156a09eebb177841d6
Merritt Krakowitzer <mkrakowitzer@fnb.co.za>
2015-08-24 09:04:13 +0200

    Add some tests

--- spec/classes/bamboo_spec.rb
+++ spec/classes/bamboo_spec.rb
@@ -7,32 +7,32 @@ describe 'bamboo' do
         let(:facts) do
           facts
         end
+        let(:params) {{
+          :javahome => '/opt/java',
+        }}
+
 
         context "bamboo class without any parameters" do
-          let(:params) {{ }}
 
           it { is_expected.to compile.with_all_deps }
-
           it { is_expected.to contain_class('bamboo::params') }
           it { is_expected.to contain_class('bamboo::install').that_comes_before('bamboo::config') }
           it { is_expected.to contain_class('bamboo::config') }
           it { is_expected.to contain_class('bamboo::service').that_subscribes_to('bamboo::config') }
 
           it { is_expected.to contain_service('bamboo') }
-          it { is_expected.to contain_package('bamboo').with_ensure('present') }
         end
       end
     end
   end
 
   context 'unsupported operating system' do
-    describe 'bamboo class without any parameters on Solaris/Nexenta' do
+    describe 'bamboo class without any parameters on Solaris' do
       let(:facts) {{
-        :osfamily        => 'Solaris',
-        :operatingsystem => 'Nexenta',
+        :kernel        => 'Solaris',
       }}
 
-      it { expect { is_expected.to contain_package('bamboo') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
+      it { expect { is_expected.to contain_package('bamboo') }.to raise_error(Puppet::Error, /Solaris not supported/) }
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+RSpec.configure do |c|
+  c.default_facts = {
+    :augeasversion    => '1.0.0',
+  }
+end
