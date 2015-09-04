a82cd92edd9c3e65fbcb8128f524c84da8b8953d
Merritt Krakowitzer <mkrakowitzer@fnb.co.za>
2015-09-05 00:43:18 +0200

    Add support for init scripts

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,12 +4,34 @@
 # It sets variables according to platform.
 #
 class bamboo::params {
-  case $::kernel {
-    'Linux': {
-      $service_name = 'bamboo'
-    }
-    default: {
-      fail("${::kernel} not supported")
+  case $::osfamily {
+    'RedHat': {
+      if $::operatingsystemmajrelease == '7' {
+        $service_file_location   = '/usr/lib/systemd/system/bamboo.service'
+        $service_file_template   = 'bamboo/bamboo.service.erb'
+        $service_lockfile        = '/var/lock/subsys/bamboo'
+      } elsif $::operatingsystemmajrelease == '6' {
+        $service_file_location   = '/etc/init.d/bamboo'
+        $service_file_template   = 'bamboo/bamboo.initscript.redhat.erb'
+        $service_lockfile        = '/var/lock/subsys/bamboo'
+      } else {
+        fail("${::operatingsystem} ${::operatingsystemmajrelease} not supported")
+      }
+    } 'Debian': {
+      if $::lsbmajdistrelease =~ /(7|12|14)/ {
+        $service_file_location   = '/etc/init.d/bamboo'
+        $service_file_template   = 'bamboo/bamboo.initscript.debian.erb'
+        $service_lockfile        = '/var/lock/bamboo'
+      } elsif $::operatingsystemmajrelease == '8' {
+        $service_file_location   = '/usr/lib/systemd/system/bamboo.service'
+        $service_file_template   = 'bamboo/bamboo.service.erb'
+        $service_lockfile        = '/var/lock/subsys/bamboo'
+      } else {
+        fail("${::operatingsystem} ${::lsbmajdistrelease} not supported")
+      }
+    } default: {
+      fail("${::osfamily} not supported")
     }
   }
+  $service_name = 'bamboo'
 }
